343d2cadc5c203b04e59286cb5f4793744f9f3b9
Gelände <markertfelix@outlook.de>
2023-11-09 21:48:25 +0100

    Funktion zum Finden d. Fragenverzeichnis erstellt

--- utils/functions.py
+++ utils/functions.py
@@ -1,10 +1,27 @@
 import json
 import os 
 
-def load_json(directory): # Nutzt Verzeichnis als Argument und gibt 
-    questions = [] # Eine leere Liste, um die geladenen Fragenobjekte zu speichern
+def gamemode_directory(gamemode): # Bestimmt passendes Verzeichnis mit Fragen anhand des gewählten Spielmodus aus
 
-    for filename in os.listdir(directory):  # Iteriere über alle Dateien im angegebenen Verzeichnis
-        with open(os.path.join(directory, filename), 'r') as file: # Vollständiger Pfad zur Datei wird erstellt 
-            data = json.load(file) 
-            return data 
+    if gamemode == "l4d_1-40":
+        mode_directory = ['l4d', '1-40']
+    elif gamemode == "l4d_41-80":
+        mode_directory = ['l4d', '41-80']
+    elif gamemode == "l4d_81-136":
+        mode_directory = ['l4d', '81-136']
+    elif gamemode == "l4d_complete":
+        print("not available yet") # Wird im späteren Verlauf integriert   
+        pass
+    elif gamemode == "xt":
+        mode_directory = ["xt"]
+    else:
+        print("Error: Mode not found")
+        mode_directory = "Error"
+    
+    act_path = os.path.dirname(os.path.abspath(__file__))
+    dict_path = os.path.dirname(act_path)
+    mode_path = [dict_path,"data","questions"]+ mode_directory
+    directory = os.path.join("/".join(mode_path))
+    return directory
+
+
