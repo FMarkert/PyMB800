3823666b91f50094d64c7967ff51166afb9448c2
Gelände <markertfelix@outlook.de>
2023-11-19 12:54:14 +0100

    Funktion, um JSONs zu konvertieren erstellt

--- utils/functions.py
+++ utils/functions.py
@@ -18,11 +18,81 @@ def gamemode_directory(gamemode): # Bestimmt passendes Verzeichnis mit Fragen an
         print("Error: Mode not found")
         mode_directory = "Error"
     
+# Bestimmt den Pfad zum Verzeichnis der Fragen:
+
     act_path = os.path.dirname(os.path.abspath(__file__))
     dict_path = os.path.dirname(act_path)
     mode_path = [dict_path,"data","questions"]+ mode_directory
     directory = os.path.join("/".join(mode_path))
     return directory
 
-def json_to_object(directory): # Wandelt JSONs zu Objekten
-    pass 
+
+
+def json_to_object(directory): # Funktion, um JSON-Dateien zu Quizfragen-Objekten zu konvertieren.
+    questions = []
+    for filename in os.listdir(directory):  # Durchläuft alle Dateien im angegebenen Verzeichnis.
+        if filename.endswith(".json"):  # Prüft, ob die Datei eine JSON-Datei ist.
+            filepath = os.path.join(directory, filename) # Erstellt den vollständigen Dateipfad.
+            try:
+                with open(filepath, 'r') as file:  # Versucht, die Datei zu öffnen.
+                    data = json.load(file)  # Lädt den JSON-Inhalt.
+
+                    # Überprüft den Fragetyp und erstellt das entsprechende Frageobjekt.
+                    if data['type'] == 'drag_drop_order':
+                        question = Drag_Drop_Order(
+                            id=data['id'],
+                            question_text=data['question_text'],
+                            src=data['src'],
+                            type=data['type'],
+                            comment=data['comment'],
+                            casestudy=data['casestudy'],
+                            items=data['items'],
+                            correct_answer=data['correct_answer']
+                        )
+                    elif data['type'] == 'drag_drop_pairs':
+                        question = Drag_Drop_Pairs(
+                            id=data['id'],
+                            question_text=data['question_text'],
+                            src=data['src'],
+                            type=data['type'],
+                            comment=data['comment'],
+                            casestudy=data['casestudy'],
+                            pairs=data['pairs']
+                        )
+                    elif data['type'] == 'dropdown':
+                        question = Dropdown(
+                            id=data['id'],
+                            question_text=data['question_text'],
+                            src=data['src'],
+                            type=data['type'],
+                            comment=data['comment'],
+                            casestudy=data['casestudy'],
+                            items=data['items'],
+                            correct_answer=data['correct_answer']
+                        )
+                    elif data['type'] == 'multiple_choice':
+                        question = Multiple_Choice(
+                            id=data['id'],
+                            question_text=data['question_text'],
+                            src=data['src'],
+                            type=data['type'],
+                            comment=data['comment'],
+                            casestudy=data['casestudy'],
+                            items=data['items'],
+                            correct_answer=data['correct_answer']
+                        )
+                    else:
+                        print(f"Unbekannter Fragetyp: {data['type']} in Datei {filename}")
+                        continue # Überspringt das Hinzufügen dieser Frage, falls der Typ unbekannt ist.
+
+                    questions.append(question)  # Fügt das erstellte Frageobjekt zur Liste hinzu.
+            
+            # Fehlerbehandlungen für verschiedene Arten von Fehlern.
+            except FileNotFoundError:
+                print(f"Datei {filename} wurde nicht gefunden.")
+            except json.JSONDecodeError:
+                print(f"Datei {filename} ist kein gültiges JSON-Format.")
+            except Exception as e:
+                print(f"Ein unerwarteter Fehler ist aufgetreten: {e}")
+
+    return questions
