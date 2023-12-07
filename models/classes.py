3feb7ed72f7637ae160ef585bd0691ffa59002c2
FMarkert <markertfelix@outlook.de>
2023-12-07 11:24:02 +0100

    Prototyp TKinter

---
+++ models/classes.py
@@ -0,0 +1,69 @@
+class Base_Question:
+    def __init__(self,id,question_text,src,type,comment,casestudy):
+        self.id = id 
+        self.question_text = question_text
+        self.src = src
+        self.type = type
+        self.comment = comment
+        self.casestudy = casestudy
+    
+    def display_question_text(self): #Methoden zum Anzeigen des Fragetextes, der Quelle, Kommentare und ggf. zugehöriger Casestudy
+        return self.question_text
+    
+    def display_src(self): 
+        return self.src
+
+    def display_comment(self):
+        return self.comment 
+    
+    def display_casestudy(self):
+        return self.casestudy
+
+
+class Drag_Drop_Order(Base_Question):
+    def __init__(self,id,question_text,src,type,comment,casestudy,items,correct_answer):
+        super().__init__(id,question_text,src,type,comment,casestudy)
+        self.items = items
+        self.correct_answer = correct_answer
+        print(f"Drag_Drop_Order Frage erstellet. Antworten = {self.correct_answer}") #Test
+    def display_c_answer(self):
+        return self.correct_answer
+    def check_answer(self, user_answer):
+        return user_answer == self.correct_answer
+
+    
+class Drag_Drop_Pairs(Base_Question):
+    def __init__(self,id,question_text,src,type,comment,casestudy,pairs):
+        super().__init__(id,question_text,src,type,comment,casestudy)
+        self.pairs = pairs 
+    def display_pairs(self):
+        return self.pairs 
+    def generate_correct_answers(self):  # Erzeuge eine Liste mit den korrekten Antworten basierend auf den Paaren
+        return [self.pairs]
+    def check_answer(self, user_answer):
+        return user_answer == self.pairs
+
+class Dropdown(Base_Question):
+    def __init__(self,id,question_text,src,type,comment,casestudy,items,correct_answer):
+        super().__init__(id,question_text,src,type,comment,casestudy)
+        self.items = items 
+        self.correct_answer = correct_answer
+        print(f"Dropdown Frage erstellt.Antworten = {self.correct_answer}")
+    def display_items(self):
+        return self.items
+    def display_c_answers(self):
+        return self.correct_answer
+    def check_answer(self, user_answer):
+        return user_answer == self.correct_answer
+        
+class Multiple_Choice(Base_Question):
+    def __init__(self,id,question_text,src,type,comment,casestudy,items,correct_answer):
+        super().__init__(id,question_text,src,type,comment,casestudy)
+        self.items = items 
+        self.correct_answer = correct_answer
+        print(f"MC Frage erstellt.Antworten = {self.correct_answer}") #Test
+    def check_answer(self, user_answer):
+        return user_answer == self.correct_answer
+
+
+
---
+++ tests/test.py
@@ -0,0 +1,13 @@
+x= {
+        "Customer Posting Group": "Specific posting group", 
+        "VAT Product Poting Group": "Tax posting groups", 
+        "VAT Business Posting Group": "Tax posting groups", 
+        "Inventory Posting Group": "Specific posting group", 
+        "Gen. Product Posting Group": "General posting group", 
+        "Vendor Posting Group": "Specific posting group", 
+        "Gen. Business Posting Group": "General posting group" 
+    } 
+
+y= {'Customer Posting Group': 'Specific posting group', 'VAT Product Poting Group': 'Tax posting groups', 'VAT Business Posting Group': 'Tax posting groups', 'Inventory Posting Group': 'Specific posting group', 'Gen. Product Posting Group': 'General posting group', 'Vendor Posting Group': 'Specific posting group', 'Gen. Business Posting Group': 'General posting group'}
+
+print(x==y)
