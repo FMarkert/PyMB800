32833c0159cbaad542882cbe7887f0346a2c7e20
FMarkert <markertfelix@outlook.de>
2023-12-16 16:47:39 +0100

    Modelle hinzugefügt

--- PyMB800_app/admin.py
+++ PyMB800_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import BaseQuestion, MultipleChoice, DropDown, DragDropOrder, DragDropPairs
 
-# Register your models here.
+admin.site.register(BaseQuestion)
+admin.site.register(MultipleChoice)
+admin.site.register(DropDown)
+admin.site.register(DragDropOrder)
+admin.site.register(DragDropPairs)
--- PyMB800_app/models.py
+++ PyMB800_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class BaseQuestion(models.Model):
+    id = models.IntegerField(primary_key=True)
+    src = models.CharField(max_length=255)
+    type = models.CharField(max_length=50)
+    question_text = models.TextField()
+    comment = models.TextField(blank=True, null=True)
+    casestudy = models.TextField(blank=True, null=True)
+
+
+class MultipleChoice(BaseQuestion):
+    items = models.JSONField()
+    correct_answer = models.JSONField()
+    def check_answer(self, user_answer):
+        return user_answer == self.correct_answer
+    
+class DropDown(BaseQuestion):
+    items = models.JSONField()
+    correct_answer = models.JSONField()
+    def check_answer(self, user_answer):
+        return user_answer == self.correct_answer
+
+class DragDropOrder(BaseQuestion):
+    items = models.JSONField()
+    correct_answer = models.JSONField()
+    def check_answer(self, user_answer):
+        return user_answer == self.correct_answer
+    
+class DragDropPairs(BaseQuestion):
+    pairs = models.JSONField()
+    def generate_correct_answers(self):  # Erzeuge eine Liste mit den korrekten Antworten basierend auf den Paaren
+        return [self.pairs]
+    def check_answer(self, user_answer):
+        return user_answer == self.pairs
