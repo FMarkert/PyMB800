72cc7799dcd638b8817cfdce1f54d743cb4590a5
FMarkert <markertfelix@outlook.de>
2023-12-16 18:48:53 +0100

    Utils hinzugefügt - noch zu testen

--- PyMB800_app/views.py
+++ PyMB800_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from PyMB800_app.utils import gamemode_directory, json_to_object
+
 
 # Ihre Startseite-View
 def index(request):
@@ -6,5 +8,12 @@ def index(request):
 
 # Die View, die aufgerufen wird, wenn der "START DEMO"-Button gedrückt wird
 def start_quiz(request):
-    # Leiten Sie hier zu Ihrer Quiz-Logik weiter
-    return redirect('quiz_page')
+    gamemode = "l4d_1-40"
+    directory = gamemode_directory(gamemode)
+    questions, errors = json_to_object(directory)
+
+    # Daten an das Template übergeben
+    return render(request, 'check_page.html', {
+        'questions': questions,
+        'errors': errors
+    })
