de75209b473ab0f62d174d4953619c7048be6c16
FMarkert <markertfelix@outlook.de>
2023-12-16 15:02:19 +0100

    Startseite hinzugefügt

--- PyMB800_app/views.py
+++ PyMB800_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+# Ihre Startseite-View
+def index(request):
+    return render(request, 'index.html')
+
+# Die View, die aufgerufen wird, wenn der "START DEMO"-Button gedrückt wird
+def start_quiz(request):
+    # Leiten Sie hier zu Ihrer Quiz-Logik weiter
+    return redirect('quiz_page')
--- pymb800/urls.py
+++ pymb800/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from PyMB800_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('start/', views.start_quiz, name='start_quiz')
 ]
